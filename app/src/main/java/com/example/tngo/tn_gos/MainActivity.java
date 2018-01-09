47f88b8650eb7db75a053c08c35543ac0963ab6d
Joan Louji <joanlouji@gmail.com>
2018-01-09 21:36:44 +0530

    First commit

---
+++ app/src/main/java/com/example/tngo/tn_gos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tngo.tn_gos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
